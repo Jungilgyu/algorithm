f69d0b3bad75491eb2a1491d7dbb971bf5856de3
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-05-10 00:38:39 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 9988 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -0,0 +1,52 @@
+// const input = require('fs').readFileSync('./input.txt', 'utf-8').trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = Number(input[0])
+
+const area = input.slice(1).map(line => line.trim().split(''));
+
+const di = [0, 1, 0, -1]
+const dj = [1, 0, -1, 0]
+
+const visited = Array.from({length: n}, () => Array(n).fill(false));
+
+function bfs(i, j) {
+	const q = []
+	q.push([i, j])
+	visited[i][j] = true
+	let res = 1
+	while (q.length > 0) {
+		const [x, y] = q.shift()
+
+		for (let k = 0; k < 4; k++) {
+			const nx = x + di[k]
+			const ny = y + dj[k]
+			if ( 0 <= nx && nx < n && 0 <= ny && ny < n && !visited[nx][ny] && area[nx][ny] === '1') {
+				visited[nx][ny] = true
+				q.push([nx, ny])
+				res += 1
+			}
+
+		}
+	}
+
+	return res
+}
+
+const answer = []
+for (let i = 0; i < n; i++) {
+	for (let j = 0; j < n; j++) {
+		if (area[i][j] === '1' && !visited[i][j]) {
+			answer.push(bfs(i, j))
+		}
+	}
+}
+
+answer.sort((a, b) => a - b)
+
+console.log(answer.length)
+for (const num of answer) {
+	console.log(num)
+}
+
+
