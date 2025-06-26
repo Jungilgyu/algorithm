d7dcb882891f6ea9408c622d9574dfefed1468ee
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-06-27 06:10:28 +0900

    [Silver I] Title: 봄버맨, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.py
+++ 백준/Silver/16918. 봄버맨/봄버맨.py
@@ -1,37 +1,28 @@
 import sys
-from collections import deque
 
 R, C, N = map(int, input().split())
+board = [list(input().strip()) for _ in range(R)]
 
-# area = [list(map(str, input())) for _ in range(R)]
-area = [list(input().strip()) for _ in range(R)]
-di = [0, 1, 0, -1]
-dj = [1, 0, -1, 0]
+def explode(board):
+    new_board = [['O'] * C for _ in range(R)]
+    for i in range(R):
+        for j in range(C):
+            if board[i][j] == 'O':
+                new_board[i][j] = '.'
+                for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    ni, nj = i + dx, j + dy
+                    if 0 <= ni < R and 0 <= nj < C:
+                        new_board[ni][nj] = '.'
+    return new_board
 
-target = deque()
-for i in range(R):
-    for j in range(C):
-        if area[i][j] == "O":
-            target.append([i, j])
+if N == 1:
+    result = board
+elif N % 2 == 0:
+    result = [['O'] * C for _ in range(R)]
+elif N % 4 == 3:
+    result = explode(board)
+else:  # N % 4 == 1
+    result = explode(explode(board))
 
-for i in range(1, N):
-    if i % 2 == 1:
-        area = [["O"] * C for _ in range(R)]
-
-    else:
-        while target:
-            x, y = target.popleft()
-            area[x][y] = "."
-            for k in range(4):
-                nx, ny = x + di[k], y + dj[k]
-                if 0 <= nx < R and 0 <= ny < C and area[nx][ny] == "O":
-                    area[nx][ny] = "."
-
-        for l in range(R):
-            for m in range(C):
-                if area[l][m] == "O":
-                    target.append([l, m])
-for x in area:
-    print("".join(map(str, x)))
-
-
+for row in result:
+    print("".join(row))
