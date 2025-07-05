55c6f4481d47c0e9590f0f5fbb5dc968760da076
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-07-05 11:50:10 +0900

    [Silver V] Title: 올림픽, Time: 32 ms, Memory: 32412 KB, Score: 12 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,10 +1,11 @@
 import sys
 input = sys.stdin.readline
+
 n, k = map(int, input().split())
 ranking = [list(map(int, input().split())) for _ in range(n)]
 
 res = sorted(ranking, key=lambda x: (x[1], x[2], x[3]), reverse=True)
-ans = 0
+ans = 1
 g, s, b = 0, 0, 0
 for x in res:
     if x[0] == k:
@@ -17,6 +18,8 @@ for x in res:
             g, s, b = x[1], x[2], x[3]
             ans += 1
 
-
-print(ans)
+if ans == 0:
+    print(1)
+else:
+    print(ans)
 
