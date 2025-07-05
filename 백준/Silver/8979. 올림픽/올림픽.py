f870f6bf2df93830be79bce585e0110298912b82
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-07-05 12:32:49 +0900

    [Silver V] Title: 올림픽, Time: 60 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,25 +1,18 @@
 import sys
-input = sys.stdin.readline
 
 n, k = map(int, input().split())
 ranking = [list(map(int, input().split())) for _ in range(n)]
 
-res = sorted(ranking, key=lambda x: (x[1], x[2], x[3]), reverse=True)
-ans = 1
-g, s, b = 0, 0, 0
-for x in res:
-    if x[0] == k:
+ranking.sort(key=lambda x: (-x[1], -x[2], -x[3]))
+
+rank = 1  
+prev = ranking[0][1:]
+
+for i in range(n):
+    country, g, s, b = ranking[i]
+    if [g, s, b] != prev:
+        rank = i + 1  # 등수 갱신: 동점자 수만큼 점프
+        prev = [g, s, b]
+    if country == k:
+        print(rank)
         break
-    else:
-        # 메달수가 다 같음
-        if g == x[1] and s == x[2] and b == x[3]:
-            continue
-        else:
-            g, s, b = x[1], x[2], x[3]
-            ans += 1
-
-if ans == 0:
-    print(1)
-else:
-    print(ans)
-
