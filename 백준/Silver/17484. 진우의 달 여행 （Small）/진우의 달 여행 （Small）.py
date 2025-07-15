0b31c268a4b93b15497d78ef2f76e5d55b5c4234
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-07-15 15:23:00 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.py
@@ -1,37 +1,27 @@
 import sys
-from collections import deque
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
 arr = [list(map(int, input().split())) for _ in range(n)]
-arr.append([-1] * m)
-# for x in arr:
-#     print(x)
-# 이동방향
-di = [1, 1, 1]
-dj = [-1, 0, 1]
+dp = [[[float('inf')] * 3 for _ in range(m)] for _ in range(n)]
 
-def sol(si, sj):
-    q = deque()
-    q.append([si, sj, arr[si][sj], -1])
-    res = []
-    while q:
-        x, y, c, prev_d = q.popleft()
 
-        # if arr[x][y] == -1:
-        #     return c
-
-        for k in range(3):
-            nx, ny = x + di[k], y + dj[k]
-            if 0 <= nx < n and 0 <= ny < m and k != prev_d:
-                q.append([nx, ny, c+arr[nx][ny], k])
-            elif nx == n:
-                res.append(c)
-
-    return res
-
-ans = []
 for j in range(m):
-    temp = sol(0, j)
-    ans.extend(temp)
-print(min(ans))
+    for d in range(3):
+        dp[0][j][d] = arr[0][j]
+
+for i in range(1, n):
+    for j in range(m):
+        for d in range(3):
+            nj = j + (d - 1)
+            if 0 <= nj < m:
+                for prev_d in range(3):
+                    if prev_d != d:
+                        dp[i][j][d] = min(dp[i][j][d], dp[i-1][nj][prev_d] + arr[i][j])
+
+ans = float('inf')
+for j in range(m):
+    for d in range(3):
+        ans = min(ans, dp[n-1][j][d])
+
+print(ans)
