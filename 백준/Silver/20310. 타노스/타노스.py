83f57e2f637867a741a59b25aabac0fe297758f0
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-07-18 16:06:25 +0900

    [Silver III] Title: 타노스, Time: 36 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,20 +1,32 @@
 import sys
-s = input()
+input = sys.stdin.readline
+s = input().rstrip()
 
+#1은 앞부터, 0은 뒤부터
+one_cnt = 0
+zero_cnt = 0
 
-# print(s)
-
-ans = ""
-zero = 0
-one = 0
 for i in range(len(s)):
     if s[i] == "0":
-        zero += 1
+        zero_cnt += 1
     else:
-        one += 1
+        one_cnt += 1
 
-for _ in range(int(zero/2)):
-    ans += "0"
-for _ in range(int(one/2)):
-    ans += "1"
-print(ans)
+one_cnt = one_cnt // 2
+zero_cnt = zero_cnt //2
+
+front = []
+for ch in s:
+    if ch == "1" and one_cnt > 0:
+        one_cnt -= 1
+    else:
+        front.append(ch)
+res = []
+for ch in reversed(front):
+    if ch == "0" and zero_cnt > 0:
+        zero_cnt -= 1
+    else:
+        res.append(ch)
+
+print(''.join(reversed(res)))
+
