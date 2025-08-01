a99c02ef714962a7486166e74ee986d7a4807ed9
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-08-01 16:01:54 +0900

    [Silver V] Title: 집합, Time: 3384 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -2,33 +2,18 @@ import sys
 input = sys.stdin.readline
 
 m = int(input())
-s = set()
+s = 0
 for _ in range(m):
-    v = input().split()
-    opr, n = -1, -1
-    if len(v) == 2:
-        opr, n = v[0], int(v[1])
-    else:
-        opr = v[0]
-
-    if opr == "add":
-        s.add(n)
-    elif opr == "remove":
-        if n in s:
-            s.remove(n)
-    elif opr == "check":
-        if n in s:
-            print(1)
-        else:
-            print(0)
-    elif opr == "toggle":
-        if n in s:
-            s.remove(n)
-        else:
-            s.add(n)
-    elif opr == "all":
-        s = {i for i in range(1, 21)}
-    else:
-        s = set()
-
-
+    cmd = input().split()
+    if cmd[0] == 'add':
+        s |= (1 << int(cmd[1]) - 1)
+    elif cmd[0] == 'remove':
+        s &= ~(1 << int(cmd[1]) - 1)
+    elif cmd[0] == 'check':
+        print(1 if s & (1 << int(cmd[1]) - 1) else 0)
+    elif cmd[0] == 'toggle':
+        s ^= (1 << int(cmd[1]) - 1)
+    elif cmd[0] == 'all':
+        s = (1 << 20) - 1
+    elif cmd[0] == 'empty':
+        s = 0
