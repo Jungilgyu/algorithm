694ae3fe1f4d599e4f67e9c622bd71e6a4f3f96e
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-08-30 12:11:26 +0900

    [Bronze I] Title: 단어 공부, Time: 72 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,26 +1,16 @@
 import sys
 input = sys.stdin.readline
 
+word = input().strip().upper()
+wword = list(set(word))
 
-word = input().strip()
+num = []
 
-count = [0] * 26
+for char in wword:
+    num.append(word.count(char))
 
-for char in word:
-    temp = ''
-    if char.islower():
-        temp = char.upper()
-    else:
-        temp = char
-    count[ord(temp) - ord('A')] += 1
-
-max_v = max(count)
-res = []
-for i in range(26):
-    if count[i] == max_v:
-        res.append(i)
-
-if len(res) >= 2:
+max_count = max(num)
+if num.count(max_count) > 1:
     print('?')
 else:
-    print(chr(res[0] + 65))
+    print(wword[num.index(max_count)])
