d7256f62a74e4f1a6c0e975de4285cf2abd14296
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-08-30 11:51:28 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -2,13 +2,4 @@ import sys
 input = sys.stdin.readline
 
 
-word = input()
-
-res = ""
-for char in word:
-    if char.isupper():
-        res += char.lower()
-    elif char.islower():
-        res += char.upper()
-
-print(res)
+print(input().swapcase())
