af01caa3303ba31420fdc96b7cfd571d95ecb0d4
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-08-31 14:20:36 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,22 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-
 word = input().strip()
 nums = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV","WXYZ"]
 
 cnt = 0
-for char in word: # 15
-    temp = 3
-    idx = 0
-    while True:
-        if char in nums[idx]:
-            cnt += temp
-            break
-        else:
-            temp += 1
-            idx += 1
-print(cnt)
+for i in range(len(word)):
+    for j in nums:
+        if word[i] in j:
+            cnt += nums.index(j) + 3
 
+print(cnt)
 
 
