dfa68037ae99f76e4fbf5aef359d21c91eb9084b
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-09-05 13:27:25 +0900

    [Gold IV] Title: 문자열 폭발, Time: 344 ms, Memory: 43596 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -2,27 +2,16 @@ import sys
 input = sys.stdin.readline
 
 word = input().rstrip('\n')
-b = list(input().rstrip('\n'))
+b = input().rstrip('\n')
 n = len(b)
 
 stack = []
-for i in range(len(word)):
-    stack.append(word[i])
-    if stack[-n:] == b:
-        for _ in range(n):
-            stack.pop()
-word = ''.join(stack)
+for ch in word:
+    stack.append(ch)
+    # 끝 글자가 폭발 문자열 끝과 같을 때만 검사
+    if ch == b[-1] and len(stack) >= n:
+        if ''.join(stack[-n:]) == b:
+            del stack[-n:]  # pop n번 대신 슬라이스 삭제
 
-print(word if word != "" else "FRULA")
-
-
-
-
-
-
-
-
-
-
-
-
+ans = ''.join(stack)
+print(ans if ans else "FRULA")
