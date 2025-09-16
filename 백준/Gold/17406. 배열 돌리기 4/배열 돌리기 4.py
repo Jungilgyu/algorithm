926959bdc4c6e719c3a248aa590929a9dcb9867b
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-09-16 14:33:58 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 288 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
@@ -1,6 +1,5 @@
 import sys
 from itertools import permutations
-import copy
 input = sys.stdin.readline
 
 n, m, k = map(int, input().split())
@@ -8,60 +7,31 @@ area = [list(map(int, input().split())) for _ in range(n)]
 command = [list(map(int, input().split())) for _ in range(k)]
 
 def rotate(r, c, s, board):
-    # (r, c) 중앙값
-    new = [[0] * m for _ in range(n)]
-    new[r-1][c-1] = board[r-1][c-1] # 중앙값
+    arr = [row[:] for row in board]
+    r -= 1
+    c -= 1
+    for d in range(1, s+1):
+        top, bottom = r-d, r+d
+        left, right = c-d, c+d
+        prev = arr[top][left]
 
-    # 방향
-    di = [0,1,0,-1]
-    dj = [1,0,-1,0]
-    d = 0
+        for col in range(left+1, right+1):
+            arr[top][col], prev = prev, arr[top][col]
+        for row in range(top + 1, bottom + 1):
+            arr[row][right], prev = prev, arr[row][right]
+        for col in range(right - 1, left - 1, -1):
+            arr[bottom][col], prev = prev, arr[bottom][col]
+        for row in range(bottom - 1, top - 1, -1):
+            arr[row][left], prev = prev, arr[row][left]
+    return arr
 
-    i, j = r-s-1, c-s-1 # 시작값
-    new[i][j] = board[i+1][j]
-    while True:
-        if i == r-1 and j == c-1:
-            break
-
-        ni, nj = i + di[d], j + dj[d]
-        if r-s-1 <= ni < r+s and c-s-1 <= nj < c+s and new[ni][nj] == 0:
-            new[ni][nj] = board[i][j]
-            i, j = ni, nj
-        else: # 방향전환
-            d = (d+1) % 4
-            ni, nj = i + di[d], j + dj[d]
-            if d == 0: # 안으로 가는 경우
-                if new[ni][nj] == 0:
-                    new[ni][nj] = board[ni+1][nj]
-            else: #그냥 방향전환만
-                new[ni][nj] = board[i][j]
-            i, j = ni, nj
-
-    for x in range(n):
-        for y in range(m):
-            if new[x][y] == 0:
-                new[x][y] = board[x][y]
-    return new
-
-
-
-cases = permutations(command)
-ans = 5001
-for case in cases:
-    res = 5001
-    temp = copy.deepcopy(area)
+ans = float('inf')
+for case in permutations(command):
+    temp = [row[:] for row in area]
     for r, c, s in case:
         temp = rotate(r, c, s, temp)
+    ans = min(ans, min(sum(row) for row in temp))
 
-    for row in temp:
-        res = min(res, sum(row))
-
-    ans = min(ans, res)
 print(ans)
 
 
-
-# ans = 5001
-# for row in area:
-#     ans = min(ans, sum(row))
-# print(ans)
