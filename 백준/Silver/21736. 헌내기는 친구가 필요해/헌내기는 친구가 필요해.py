5cea511d1638fffc72e30cbf12eefbe033bdb3a7
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-10-26 13:16:05 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 400 ms, Memory: 41368 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -8,27 +8,38 @@ room = [list(input().strip()) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
 di = [0, 1, 0, -1]
 dj = [1, 0, -1, 0]
-ans = 0
 
-def dfs(i, j):
-    global ans
 
-    if room[i][j] == 'P':
-        ans += 1
+def bfs(i, j):
+    q = deque()
+    q.append([i, j])
+    visited[i][j] = True
 
-    for k in range(4):
-        ni, nj = i + di[k], j + dj[k]
-        if 0 <= ni < n and 0 <= nj < m and not visited[ni][nj] and room[ni][nj] != 'X':
-            visited[ni][nj] = True
-            dfs(ni, nj)
+    res = 0
+    while q:
+        ci, cj = q.popleft()
+
+        if room[ci][cj] == 'P':
+            res += 1
+
+        for k in range(4):
+            ni, nj = ci + di[k], cj + dj[k]
+            if 0 <= ni < n and 0 <= nj < m and not visited[ni][nj] and room[ni][nj] != 'X':
+                q.append([ni, nj])
+                visited[ni][nj] = True
+
+    return res
+
 
 si, sj = -1 ,-1
+ans = 0
 for i in range(n):
     for j in range(m):
         if room[i][j] == 'I':
             si, sj = i, j
-            dfs(si, sj)
+            ans = bfs(si, sj)
             break
+
 if ans == 0:
     print("TT")
 else:
