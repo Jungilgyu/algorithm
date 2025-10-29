6a7b7c6921e20f4544f87330a71d7b3a922017e9
정일규 <127087955+Jungilgyu@users.noreply.github.com>
2025-10-29 12:17:43 +0900

    [level 2] Title: 타겟 넘버, Time: 315.25 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,21 +1,10 @@
-answer = 0
-def dfs(idx, numbers, current, target, n):
-    global answer
-    if idx == n-1:
-        if current == target:
-            answer += 1
-        return
+def dfs(idx, current, numbers, target, n):
+    if idx == n:
+        return 1 if current == target else 0
     
-    idx += 1
-    
-    dfs(idx, numbers, current + numbers[idx], target, n)
-    dfs(idx, numbers, current - numbers[idx], target, n)
-
+    return (dfs(idx+1, current + numbers[idx], numbers, target, n)
+            + dfs(idx+1, current - numbers[idx], numbers, target, n))
 
 def solution(numbers, target):
-    global answer
     n = len(numbers)
-    dfs(0, numbers, numbers[0], target, n)
-    dfs(0, numbers, -numbers[0], target, n)
-    
-    return answer
+    return dfs(0, 0, numbers, target, n)
